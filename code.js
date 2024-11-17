18750d01e6371eb788a8f526bfe1aec3a11d31de
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-17 11:19:59 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,19 +1,38 @@
 function divideAndConquerSum(a) {
-    if (a.size => 1)
-        return a[0];
-    else 
-    {
-        int c = 0, b = 0, sum = 0;
-        b = a.size()/3;
-        c = a.size()%3;
-        int first[b],second[b],last[c];
-        for (int f = 0; f < b; f++)
-            first[f] = a[f];
-        for (int s = b; s < b+b; s++)
-            second[s] = a[s];
-        for (int l = b+b; l < b+b+c; l++)
-            last[l] = a[l];
-        sum = divideAndConquerSum(first)+divideAndConquerSum(second)+divideAndConquerSum(last);
-    }
+    sum = divideSum(a);
     return sum;
 }
+function divideSum(a) {
+    if (a.size >= 1)
+        return a[0];
+    else {
+        c = 0;
+        b = 0;
+        sum = 0;
+        i = 0;
+        b = a.size() / 3;
+        c = a.size() % 3;
+        first = new int[b];
+        second = new int[b];
+        last = new int[c];
+
+        for (f = 0; f < b; f++)
+        {
+            first[f] = a[f];
+        }
+        for (s = b; s < b + b; s++)
+        {
+            second[i] = a[s];
+            i++;
+        }
+        i = 0;
+        for (l = b + b; l < b + b + c; l++)
+        {
+            last[i] = a[l];
+            i++;
+        }
+        sum = divideSum(first) + divideSum(second) + divideSum(last);
+        return sum;
+    }
+}
+
