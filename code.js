b7d78182d9a6c0a3a26b71a97789add0112ae47a
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-17 10:27:55 -0700

    Update code.js

--- code.js
+++ code.js
@@ -9,9 +9,9 @@ function divideAndConquerSum(a) {
         int first[b],second[b],last[c];
         for (int f = 0; f < b; f++)
             first[f] = a[f];
-        for (int s = b-1; s < b+b; s++)
+        for (int s = b; s < b+b; s++)
             second[s] = a[s];
-        for (int l = b+b-1; l < b+b+c; l++)
+        for (int l = b+b; l < b+b+c; l++)
             last[l] = a[l];
         sum = divideAndConquerSum(first)+divideAndConquerSum(second)+divideAndConquerSum(last);
     }
