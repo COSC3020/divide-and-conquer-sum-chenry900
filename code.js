219a36eba179752c64815cfbc0a2f44e2ad579a5
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-20 21:14:25 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function divideAndConquerSum(a) {
-    sum = divideSum(a);
+    var sum = divideSum(a);
     return sum;
 }
 function divideSum(a) {
@@ -9,7 +9,7 @@ function divideSum(a) {
         return 0;
     else {
         var c = a.length % 3;
-        var b = a.length / 3;
+        var b = math.floor(a.length / 3);
         var sum = 0;
         var first = a.slice(0,b);
         var second = a.slice(b, b+b);
