d2d4242052f86ff8ed4cd817e1d6e35fff97473e
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-20 21:25:19 -0700

    Update code.js

--- code.js
+++ code.js
@@ -8,11 +8,10 @@ function divideSum(a) {
     else if (a.length == 0)
         return 0;
     else {
-        var c = a.length % 3;
         var b = Math.floor(a.length / 3);
-        var first = a.slice(0,b);
-        var second = a.slice(b, b+b);
-        var last = a.slice(b + b, b + b + c);
+        var first = a.slice(0, b);
+        var second = a.slice(b, b + b);
+        var last = a.slice(b + b, a.length);
         sum = divideSum(first) + divideSum(second) + divideSum(last);
         return sum;
     }
