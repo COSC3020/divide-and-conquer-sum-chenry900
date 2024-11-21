53d1f027b542871cf8a2f5bc3acd43c63969c18d
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-20 20:47:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -3,34 +3,17 @@ function divideAndConquerSum(a) {
     return sum;
 }
 function divideSum(a) {
-    if (a.length() >= 1)
+    if (a.length == 1)
         return a[0];
+    else if (a.length == 0)
+        return 0;
     else {
-        c = 0;
-        b = 0;
-        sum = 0;
-        i = 0;
-        b = a.length() / 3;
-        c = a.length() % 3;
-        first = new int[b];
-        second = new int[b];
-        last = new int[c];
-
-        for (f = 0; f < b; f++)
-        {
-            first[f] = a[f];
-        }
-        for (s = b; s < b + b; s++)
-        {
-            second[i] = a[s];
-            i++;
-        }
-        i = 0;
-        for (l = b + b; l < b + b + c; l++)
-        {
-            last[i] = a[l];
-            i++;
-        }
+        var c = a.length % 3;
+        var b = a.length / 3;
+        var sum = 0;
+        var first = a.slice(0,b);
+        var second = a.slice(b, b+b);
+        var last = a.slice(b + b, b+b+c);
         sum = divideSum(first) + divideSum(second) + divideSum(last);
         return sum;
     }
